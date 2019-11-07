0d69a13c048b9e2bbe83b74739e176b4527f1939
Ryanflima <ryanf363@gmail.com>
2019-11-07 16:04:18 -0500

    did some changes, so the terminal can give the answer if the IP is ok

--- Blacklist.js
+++ Blacklist.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const unirest = require('unirest');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -11,11 +12,26 @@ app.use(express.static('./public'));
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function(req, res){
-    res.render( 'home.ejs', { IP: null, error: null } );
+    res.render( 'home.ejs', { ip: null, error: null } );
   });
 
 app.post('/', function(req, res) {
-    let url = "https://tony11-blacklist-ip-v1.p.rapidapi.com/ipv4/%7Bip%7D"
+    var options = {
+        method: 'GET',
+        url: 'https://tony11-blacklist-ip-v1.p.rapidapi.com/ipv4/'+ req.body.IP,
+        headers: {
+          'x-rapidapi-host': 'tony11-blacklist-ip-v1.p.rapidapi.com',
+          'x-rapidapi-key': '00120c5efamshf7d1053fdbaa4d0p112cdajsn59ac94ed1cc3'
+        }
+      };
+      
+      request(options, function (error, response, body) {
+          if (error) throw new Error(error);
+          let status =   JSON.parse(body).status
+            console.log(status)
+          res.render( 'home.ejs', { ip: status, error: null } );   
+          
+      });
 })
 
 
